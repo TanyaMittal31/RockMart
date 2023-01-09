902cc0d6ff6b56566db64feff19ad78a04596657
Tanya Agrawal <tanyaagrawal2409@gmail.com>
2023-01-09 15:07:56 +0530

    Add project files.

---
+++ Rock.Mart.Products/DbContexts/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rock.Mart.Products.Models;
+
+namespace Rock.Mart.Products.DbContexts
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
